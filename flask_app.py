f6e3d22b46ddd09c750bd21de62c739006375095
Pouya-Abikari <99250043+Pouya-Abikari@users.noreply.github.com>
2022-05-27 16:41:06 +0100

    Create flask_app.py

---
+++ flask_app.py
@@ -0,0 +1,47 @@
+
+# A very simple Flask Hello World app for you to get started with...
+
+from flask import Flask, render_template, request
+import sqlite3
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('signup.html')
+
+@app.route('/signup' , methods=['POST'])
+def signup():
+    con = sqlite3.connect('login.db')
+    cur = con.cursor()
+    cur.execute("INSERT INTO user(username, password) VALUES (?,?)",
+                    (request.form['un'],request.form['pw']))
+    con.commit()
+    return 'user added'
+@app.route('/create')
+def create():
+    con = sqlite3.connect('login.db')
+    cur = con.cursor()
+    cur.execute("""CREATE TABLE user(
+        username VARCHAR(20) NOT NULL PRIMARY KEY,
+        password VARCHAR(20) NOT NULL)
+        """)
+    return 'tbl created'
+
+@app.route('/insert')
+def insert():
+    con = sqlite3.connect('login.db')
+    cur = con.cursor()
+    cur.execute("""INSERT INTO user(username, password)
+                    VALUES ("bob" , "123")
+        """)
+    con.commit()
+    return 'insert!'
+
+@app.route('/select')
+def select():
+    con = sqlite3.connect('login.db')
+    cur = con.cursor()
+    cur.execute("SELECT * FROM user")
+    rows = cur.fetchall()
+    return str(rows)
