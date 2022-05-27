f1f688b1426bafdb4db30c57ddbb096ca0f844bf
Pouya-Abikari <99250043+Pouya-Abikari@users.noreply.github.com>
2022-05-27 16:51:06 +0100

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -28,6 +28,18 @@ def create():
         """)
     return 'tbl created'
 
+@app.route('/login', methods=['POST'])
+def login():
+    con = sqlite3.connect('login.db')
+    cur = con.cursor()
+    cur.execute("SELECT * FROM Users WHERE Username=? AND Password=?",
+                    (request.form['un'],request.form['pw']))
+    match = len(cur.fetchall())
+    if match == 0:
+        return "Wrong username and password"
+    else:
+        return "Welcome " + request.form['un']
+  
 @app.route('/insert')
 def insert():
     con = sqlite3.connect('login.db')
